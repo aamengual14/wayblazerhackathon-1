14fc328963ffd5a66bc6a568de819e7d2ad3adc6
Alfredo Amengual <amengual.alfredo@gmail.com>
2016-07-09 13:06:53 -0500

    required request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+
+// request('http://www.google.com', function (error, response, body) {
+//   if (!error && response.statusCode == 200) {
+//     console.log(body) // Show the HTML for the Google homepage.
+//   }
+// })
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
