4c1d03e6bee7549e78ac5b5fe7d08de193aa4486
Christian Mansour <c.mansour@miami.edu>
2016-07-26 18:44:08 -0500

    added a calender and took off the form labels

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
   $( document ).ready(function() {
 
-    $( function() {
-      $( "#datepicker" ).datepicker();
+    $('.datepicker').datepicker({
+      inline: true,
+      showOtherMonths: true,
+      dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
     });
 
     var conceptsArray = [];
